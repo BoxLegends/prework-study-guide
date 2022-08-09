ffaa3a014304140732f91b43813e8f1d0b0d8971
Nick Burns <Nick_burns11@hotmail.com>
2022-08-09 15:45:24 -0400

    added javaScript

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+
+var topics = ['HTML','CCS','Git','JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+  
+    console.log(topics[x]);
+  
+}
+
+/*
+if (topic === 'HTML') {
+
+    console.log('Lets Study HTML!');
+
+} else if (topic === 'CSS') {
+
+    console.log("Lets study CSS!");
+
+} else if (topic === 'Git') {
+
+    console.log("Lets study Git!");    
+
+} else if (topic === 'JavaScript') {
+
+    console.log("Lets study JavaScript!");
+
+} else{
+
+    console.log("Please try again!");
+
+}
+*/
